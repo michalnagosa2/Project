3ad5d2cad4297144efd166f2fe6f24eab77cb663
Michal Nagosa <michalnagosa1821@gmail.com>
2020-02-09 10:10:12 +0200

    deployment file

---
+++ deploy.groovy
@@ -0,0 +1,37 @@
+node('linux'){
+def customImage = ""
+environment{
+    registry = 'michalnagosa96/project'
+    registryCredential = 'docker.michal,nagosa96'
+    customImage = ''
+}
+stage('git'){
+git branch: 'master',
+url:'https://github.com/michalnagosa2/flask-http.git'
+}
+stage("create dockerfile") {
+sh """
+tee Dockerfile <<-'EOF'
+FROM alpine:latest
+RUN apk update && \
+    apk add  python3 
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /app/requirements.txt
+WORKDIR /app
+RUN pip3 install -r requirements.txt
+COPY . /app
+ENTRYPOINT [ "python3" ]
+CMD [ "app.py" ]
+EOF
+"""
+}
+stage('Build image') {
+customImage = docker.build("jenkins/opsschool-jenkins-test-01:tag")
+}
+stage("Push image") {
+docker.withRegistry('https://registry-1.docker.io/v1', 'docker.michal,nagosa96') {
+customImage.push()
+}
+}
+}
+	
