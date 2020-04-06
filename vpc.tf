048233ea1c986854743a2a5914fa9d9642c9d88d
michalnagosa2 <41059985+michalnagosa2@users.noreply.github.com>
2020-04-06 20:34:46 +0300

    fix port 5000

--- vpc.tf
+++ vpc.tf
@@ -64,7 +64,7 @@ resource "aws_instance" "jenkins_master" {
   }
   provisioner "remote-exec" {
     inline = [
-      "sudo docker run -d -p 8080:8080 -p 50000:50000 -v ${local.jenkins_home_mount} -v ${local.docker_sock_mount} --env ${local.java_opts} jenkins/jenkins"
+      "sudo docker run -d -p 8080:8080 -p 5000:5000 -v ${local.jenkins_home_mount} -v ${local.docker_sock_mount} --env ${local.java_opts} jenkins/jenkins"
     ]
   }
 }
