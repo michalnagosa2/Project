685136da468ff9352e467989f17df8a591e8cbab
michalnagosa2 <41059985+michalnagosa2@users.noreply.github.com>
2020-06-07 14:16:44 +0300

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -10,7 +10,7 @@ git branch: 'master',
 url:'https://github.com/michalnagosa2/flask-http.git'
 }
 stage('Build image') {
-customImage = docker.build( '- t jenkins/flask-http .')
+customImage = docker.build( "- t jenkins/flask-http .")
 }
 stage("Push image") {
 docker.withRegistry('https://registry-1.docker.io/v1', 'docker.michal,nagosa96') {
